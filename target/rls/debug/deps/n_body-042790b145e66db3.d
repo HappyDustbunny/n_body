d58cbecd56b7bec8e9388b6232a679d52d2e3662
Mads Horn <madshorn@operamail.com>
2019-07-08 11:56:01 +0200

    Trying to set project up.

---
+++ target/rls/debug/deps/n_body-042790b145e66db3.d
@@ -0,0 +1,5 @@
+C:\Users\Mads Horn\RustProjects\n_body\target\rls\debug\deps\n_body-042790b145e66db3.rmeta: src\main.rs
+
+C:\Users\Mads Horn\RustProjects\n_body\target\rls\debug\deps\n_body-042790b145e66db3.d: src\main.rs
+
+src\main.rs:
---
+++ target/rls/debug/deps/n_body-9640f1004fbf836b.d
@@ -0,0 +1,5 @@
+C:\Users\Mads Horn\RustProjects\n_body\target\rls\debug\deps\n_body-9640f1004fbf836b.rmeta: src\main.rs
+
+C:\Users\Mads Horn\RustProjects\n_body\target\rls\debug\deps\n_body-9640f1004fbf836b.d: src\main.rs
+
+src\main.rs:
