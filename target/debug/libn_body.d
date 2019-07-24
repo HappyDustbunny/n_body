a97d36be4498ec27c2f0d36aab9c28c901964725
Mads Horn <madshorn@operamail.com>
2019-07-24 12:55:01 +0200

    Tried to add a .gitignore and stop tracking Cargo.lock and /target

--- target/debug/libn_body.d
+++
@@ -1,1 +0,0 @@
-/home/horn/Documents/Rust/n_body/target/debug/libn_body.rlib: /home/horn/Documents/Rust/n_body/src/hns.rs /home/horn/Documents/Rust/n_body/src/lib.rs
--- target/debug/n_body.d
+++
@@ -1,1 +0,0 @@
-/home/horn/Documents/Rust/n_body/target/debug/n_body: /home/horn/Documents/Rust/n_body/src/hns.rs /home/horn/Documents/Rust/n_body/src/lib.rs /home/horn/Documents/Rust/n_body/src/main.rs
